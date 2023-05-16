5dfe4ba0f07c3b3bc2f00bdb0ca6af91e67b9ea6
antonioclaudio07 <acf07112002@gmail.com>
2023-05-16 19:35:11 -0300

    Mudança no database da atividade prática 11

--- Trabalhos/Ciclo 02/Repositório GitHub/Atividades práticas/Semana 10, 11/database.js
+++ Trabalhos/Ciclo 02/Repositório GitHub/Atividades práticas/Semana 10, 11/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('filmes_database', 'root', 'root', {
+const sequelize = new Sequelize('filmes_database', 'root', 'Acf@07112002', {
     host: "localhost",
     dialect: "mysql"
 });
