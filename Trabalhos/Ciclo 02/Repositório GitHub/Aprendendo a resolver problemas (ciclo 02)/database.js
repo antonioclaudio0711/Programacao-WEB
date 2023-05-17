830d1d3875d134c27d23a22095bc9255c8d43c54
antonioclaudio07 <acf07112002@gmail.com>
2023-05-16 21:21:08 -0300

    Adição de elementos da APR 02

--- Trabalhos/Ciclo 02/Repositório GitHub/Aprendendo a resolver problemas (ciclo 02)/database.js
+++
@@ -1,8 +0,0 @@
-const {Sequelize} = require('sequelize');
-
-const sequelize = new Sequelize('banco_de_dados_arp01', 'root', 'root', {
-    host: "127.0.0.1",
-    dialect: "mysql",
-});
-
-module.exports = sequelize;
