cc47b611d79eacc55b62858e8e486ab1431376ff
antonioclaudio07 <acf07112002@gmail.com>
2023-05-17 08:19:37 -0300

    Mudança no host presente no arquivo database

--- Trabalhos/Ciclo 02/Repositório GitHub/Atividades práticas/Semana 10, 11/database.js
+++ Trabalhos/Ciclo 02/Repositório GitHub/Atividades práticas/Semana 10, 11/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('filmes_database', 'root', 'Acf@07112002', {
-    host: "localhost",
+const sequelize = new Sequelize('filmes_database', 'root', 'root', {
+    host: "127.0.0.1",
     dialect: "mysql"
 });
 
